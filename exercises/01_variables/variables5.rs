28b24daf6b02166e8da8eeec52dd0c19195e80cd
Gregory Peterson <pet03356@morris.umn.edu>
2024-02-29 10:07:11 -0600

    ak.jsdfh

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -8,6 +8,11 @@
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    {
+    let number = "F-I-V-E"; // don't rename this variable
+    println!("Number plus two is : {}", number);
+    }
+
+    let mut num = 3;
+    let num =4;
 }
